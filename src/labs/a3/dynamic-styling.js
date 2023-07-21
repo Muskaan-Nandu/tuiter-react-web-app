92add4f9f956a1e44e0a7cff1fa8c496f0231dad
Muskaan Nandu <nandumuskaan@gmail.com>
2023-07-21 00:46:33 -0400

    Assn3 Lab done

--- src/labs/a3/dynamic-styling.js
+++
@@ -1,8 +0,0 @@
-function DynamicStyling() {
- return(
-  <div>
-   <h1>Hello World!</h1>
-  </div>
- )
-};
-export default DynamicStyling;
--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -1,12 +1,20 @@
 import JavaScript from "./java-script";
 import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
+import TodoItem from "./todo/todo-item";
 
 function Assignment3() {
    return(
       <div>
          <h1>Assignment 3</h1>
+         <ConditionalOutput/>
+         <Styles/>
          <Classes/>
          <JavaScript/>
+         <TodoItem/>
+         <TodoList/>
       </div>
    );
 }
