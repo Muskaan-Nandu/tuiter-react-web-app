9c60a50dc91adf08fd9aa19f93f6b4141ec244e5
Muskaan Nandu <nandumuskaan@gmail.com>
2023-08-07 18:04:32 -0400

    Corrections 3

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
+const API_BASE = process.env.REACT_APP_SERVER_API_URL;
+console.log("auth-service SERVER_API_URL: ", API_BASE);
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const findTuits = async () => {
